6a54123ca7bb418c8e529131fae745a2ed664786
YouHao <w00010110810@gmail.com>
2022-07-22 22:25:50 +0800

    first

---
+++ script.js
@@ -0,0 +1,27 @@
+// function Myfunction(){
+//     var a=document.getElementById("title");
+//     console.log(a.value)
+    
+// }
+
+
+// var title = document.getElementById("title");
+// var unit = document.getElementById("unit");
+// var content = document.getElementById("content");
+// var btn = document.getElementById("btn");
+
+// btn.addEventListener("click",function(){
+//     var str = `
+//     <div>
+//         <h2>${title.value}</h2>
+//         <p>${unit.value}</p>
+//         <p>${content.value}</p>
+//     </div>
+//     `;
+//     console.log(str);
+// });
+
+let a=document.querySelectorAll(".data_value");
+console.log(a[0].value);
+
+
